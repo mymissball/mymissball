eb610657dbb330e648acee26d8c9225428d79caf
unknown <9791@mail.com>
2021-12-09 11:33:34 +0800

    '2'

--- demo/src/main/java/com/example/demo/iFile/IFile.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.iFile;
-
-import java.io.File;
-
-public interface IFile {
-    public void saveFile(File file ,String path);
-    public void  readFile(File file);
-    public void traver(File file);
-}
